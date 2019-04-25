4f5294c73561e70dc3e0c5c88346cbb880bffc94
Ray Dev <radmedov@gmail.com>
2019-04-24 18:09:02 -0700

    [UPD] Remove apollo boost

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -16,12 +16,10 @@ import { cookiesEnabled } from '@shopify/theme-cart';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
-// import ApolloClient from 'apollo-boost';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
-// import { mountReact } from '../../react/mountReact';
 import App from '../../react/components/App';
 // Apollo client setup
 const httpLink = createHttpLink({
